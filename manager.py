99f86dfbcff587796b687bb222ea2b3ceb5560b6
elleryq <elleryq@gmail.com>
2014-04-17 18:32:18 +0800

    Add Celery support.
     * Still implementing

--- manager.py
+++ manager.py
@@ -5,12 +5,36 @@ from filesystem import Folder, File
 from action import *
 from flask import request
 from os import error
+from celery import Celery
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
 
 app = Flask(__name__)
 app.config.update(
     DEBUG=True,
     FILES_ROOT=os.path.dirname(os.path.abspath(__file__)),
+    CELERY_BROKER_URL='sqla+sqlite:///celerydb.sqlite',
+    CELERY_RESULT_BACKEND='db+sqlite:///results.sqlite',
 )
+celery = make_celery(app)
+
+
+@celery.task()
+def copy_log(src, dst):
+    # TODO: need long time.
+    return True
+
 
 @app.route('/')
 @app.route('/files/<path:path>')
