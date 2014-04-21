dc92d756ccb3337a5b2f52dd89fdcab3883a6d36
elleryq <elleryq@gmail.com>
2014-04-21 16:31:30 +0800

    Change layout to single view.
     * Now the view shows both files and folders.

--- filesystem.py
+++ filesystem.py
@@ -8,22 +8,22 @@ import string
 
 class PaginationList(list):
     def get_page(self, page=1, per_page=10):
-        if page<1:
+        if page < 1:
             raise Exception("Spcified page is not existed.")
         start = (page-1)*per_page
 
         count = len(self)
         end = page*per_page
-        if start>count:
+        if start > count:
             raise Exception("Spcified page is not existed.")
-        if end>count:
+        if end > count:
             end = count
         return self[start:end]
 
 
 class Node(object):
     def __init__(self, root, path):
-        splitetPath = string.split(path,"/")
+        splitetPath = string.split(path, "/")
         self.path = os.path.sep.join(splitetPath)
         self.root = root
         self._basename = basename(self.path)
@@ -38,8 +38,16 @@ class Node(object):
         action = action_class(self)
         return action.apply()
 
+    def isdir(self):
+        return isdir(join(self.root, self.path))
+
+    def isfile(self):
+        return isfile(join(self.root, self.path))
+
+
 class File(Node):
     avaliable_actions = [View, ]
+
     def __unicode__(self):
         return self.name
 
@@ -54,14 +62,16 @@ class File(Node):
     def get_path(self):
         return dirname(self.path)
 
+
 class Folder(Node):
-    
+
     def __init__(self, root, path):
         super(Folder, self).__init__(root, path)
         # self.files = []
         # self.folders = []
         self.files = PaginationList()
         self.folders = PaginationList()
+        self.nodes = PaginationList()
 
     @property
     def name(self):
@@ -73,11 +83,13 @@ class Folder(Node):
             chunk_path = join(chunk_path, chunk)
             yield {'chunk': chunk, 'path': chunk_path}
 
-
     def read(self):
-        for node in os.listdir(join(self.root, self.path)):
-            full_path = join(self.path, node)
+        for entry in os.listdir(join(self.root, self.path)):
+            full_path = join(self.path, entry)
             if isdir(join(self.root, full_path)):
-                self.folders.append(Folder(self.root, full_path))
+                node = Folder(self.root, full_path)
+                self.folders.append(node)
             if isfile(join(self.root, full_path)):
-                self.files.append(File(self.root, full_path))
+                node = File(self.root, full_path)
+                self.files.append(node)
+            self.nodes.append(node)
--- manager.py
+++ manager.py
@@ -10,7 +10,7 @@ from flask.ext.script import Manager
 
 app = Flask(__name__)
 app.config.update(
-    DEBUG=True,
+#    DEBUG=True,  # This line cause run fail, why??
     FILES_ROOT=os.path.dirname(os.path.abspath(__file__)),
 )
 manager = Manager(app)
@@ -31,23 +31,26 @@ def index(path=''):
             files_page = 1
         folder = Folder(app.config['FILES_ROOT'], path)
         folder.read()
-        return render_template('folder.html', folder=folder)
+        # return render_template('folder.html', folder=folder)
+        return render_template('single.html', folder=folder)
     else:
         my_file = File(app.config['FILES_ROOT'], path)
         context = my_file.apply_action(View)
         folder = Folder(app.config['FILES_ROOT'], my_file.get_path())
-        if context == None:
+        if context is None:
             return render_template('file_unreadable.html', folder=folder)
         return render_template('file_view.html', text=context['text'], file=my_file, folder=folder)
+
 
 @app.route('/search', methods=['POST'])
 def search():
     q = request.form['q']
-    return render_template('search.html', request = q)
+    return render_template('search.html', request=q)
+
 
 @app.route('/new_directory', methods=["POST"])
 @app.route('/<path:path>/new_directory', methods=["POST"])
-def create_directory(path = "/"):
+def create_directory(path="/"):
     dirname = request.form["new_directory_name"]
     directory_root = request.form["directory_root"]
     full_path = os.path.join(directory_root, dirname)
@@ -56,9 +59,8 @@ def create_directory(path = "/"):
     except error:
         pass
     return redirect('/files/' + directory_root)
-    
 
 
 if __name__ == '__main__':
-    #app.run(host="0.0.0.0")
-    manager.run()
+    app.run(host="0.0.0.0")
+    #manager.run() # it also cause error, why??
